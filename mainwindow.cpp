4d4da2ba8be0283f01d03aa1cdf5080a66cb2cb2
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2023-10-06 20:30:46 +0300

    First primitive server implementation
    Strongly borrows from the Fortune Server example.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,6 @@
+#include <QtNetwork>
+#include <QMessageBox>
+
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
@@ -9,14 +12,74 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->listenButton, SIGNAL(clicked()), this, SLOT(handleButtonPress()));
 }
 
-void MainWindow::handleButtonPress()
-{
-    QString text = ui->portEdit->text();
-    qDebug() << "Button pressed: " << text;
-}
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
+void MainWindow::handleButtonPress()
+{
+    QString text = ui->portEdit->text();
+    qDebug() << "Button pressed: " << text;
+    initServer();
+    connect(tcpServer, &QTcpServer::newConnection, this, &MainWindow::newConnection);
+}
+
+
+void MainWindow::newConnection()
+{
+    qDebug() << "New connection";
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_10);
+
+    out << "Jepeti jee";
+
+    QTcpSocket *clientConnection = tcpServer->nextPendingConnection();
+    connect(clientConnection, &QAbstractSocket::disconnected,
+            clientConnection, &QObject::deleteLater);
+
+    int rowIndex = ui->connectionsTable->rowCount();
+    ui->connectionsTable->insertRow(rowIndex);
+    QTableWidgetItem *addrItem = new QTableWidgetItem(tr("%1/%2")
+                .arg(clientConnection->peerAddress().toString())
+                .arg(clientConnection->peerPort()));
+    ui->connectionsTable->setItem(rowIndex, 0, addrItem);
+    QTableWidgetItem *bytesItem = new QTableWidgetItem("0");
+    ui->connectionsTable->setItem(rowIndex, 1, bytesItem);
+
+    clientConnection->write(block);
+    clientConnection->disconnectFromHost();
+}
+
+
+void MainWindow::initServer()
+{
+    tcpServer = new QTcpServer(this);
+    if (!tcpServer->listen()) {
+        QMessageBox::critical(this, tr("Fortune Server"),
+                              tr("Unable to start the server: %1.")
+                              .arg(tcpServer->errorString()));
+        close();
+        return;
+    }
+
+    QString ipAddress;
+    QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
+    // use the first non-localhost IPv4 address
+    for (int i = 0; i < ipAddressesList.size(); ++i) {
+        if (ipAddressesList.at(i) != QHostAddress::LocalHost &&
+            ipAddressesList.at(i).toIPv4Address()) {
+            ipAddress = ipAddressesList.at(i).toString();
+            break;
+        }
+    }
+    // if we did not find one, use IPv4 localhost
+    if (ipAddress.isEmpty())
+        ipAddress = QHostAddress(QHostAddress::LocalHost).toString();
+
+    ui->ipAddrLabel->setText(ipAddress);
+    ui->portEdit->setText(QString::number(tcpServer->serverPort()));
+}
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
+class QTcpServer;
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,8 +18,12 @@ public:
 
 public slots:
     void handleButtonPress();
+    void newConnection();
 
 private:
+    void initServer();
+
     Ui::MainWindow *ui;
+    QTcpServer *tcpServer = nullptr;
 };
 #endif // MAINWINDOW_H
